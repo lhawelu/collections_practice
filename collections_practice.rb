97799a8090f01b04cd8319a5cf75e7a5d4179458
Larry Hawelu <lhawelu@gmail.com>
2021-04-07 15:17:35 -0700

    commit tree 5925e1925f9df85159e6c44406ba28ef43c513b0

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
 
+def sort_array_asc array
+  array.sort()
+end
+
+def sort_array_desc array
+  array.sort().reverse
+end
+
+def sort_array_char_count array
+  array.sort_by{|word| word.length}
+end
+
+def swap_elements array
+  array[1, 2] = array.values_at(2, 1)
+  array
+end
+
+def reverse_array array
+  array.reverse
+end
+
+def kesha_maker array
+  array.each {|word| word[2] = "$"}
+end
+
+def find_a array
+  array.select {|word| word.start_with?("a")}
+end
+
+def sum_array array
+  array.reduce(:+)
+end
+
+def add_s array
+  array.each_with_index.collect{|word, index| index != 1? word + "s" : word }
+end
+
